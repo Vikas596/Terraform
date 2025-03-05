bf0871b7e0d58678512fac573d3591e274c263e5
Vikas596 <yadavvikasnarayan@gmail.com>
2025-03-05 20:43:53 +0530

    this is the example of types of variable

--- hello_variable.tf
+++ hello_variable.tf
@@ -1,4 +1,13 @@
-output printname {
-    value = "Your name is ${var.username} and your age is ${var.age}"
+# output printname {
+#     value = "Your name is ${var.username} and your age is ${var.age}"
   
+# }
+
+# ---------------------------- 4
+
+output printfist {
+    value = "First user is ${var.user[0]}"
 }
+
+
+
--- variable.tf
+++ variable.tf
@@ -1,19 +1,43 @@
-# This the example of variable which not default
+# This the example of variable which not default - 1
 
 # variable username {
   
 # } 
 
-# This is example of variable which is default
+# ----------------------------------------------------------------------
 
-variable username {
+# This is example of variable which is default -2
 
-    default = "Vikas"
+# variable username {
+
+#     default = "Vikas"
   
-}
+# }
 
-variable age {
+# variable age {
     
-}
+# }
 
 # We can also use variable in not interectve mode by terraform plan -var age="30"
+
+# ----------------------------------------------------------------------------------------
+
+# This is the example of types of variable - 3
+
+# variable username {
+#     type = string
+#     default = "Vikas"
+  
+# }  ----- this is the example of string
+
+# variable age {
+#     type = number
+# }  ----- this is the example of number
+
+# This is the example of List type variable - 4
+
+variable user {
+
+    type = list
+  
+}
