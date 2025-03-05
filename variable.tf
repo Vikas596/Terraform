da1d267da2729408308e17b7ad212386c727bba3
Vikas596 <yadavvikasnarayan@gmail.com>
2025-03-05 20:22:07 +0530

    this is the example of default variable

--- variable.tf
+++ variable.tf
@@ -1,3 +1,13 @@
+# This the example of variable which not default
+
+# variable username {
+  
+# } 
+
+# This is example of variable which is default
+
 variable username {
+
+    default = "world , vikas"
   
 }
